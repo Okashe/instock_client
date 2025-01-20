a5fd3fa9cdfe84323922939d532302ae77a50879
Okashe <okashabdi88@gmail.com>
2025-01-20 15:01:30 -0600

    correct path

--- src/components/FormError/FormError.jsx
+++ src/components/FormError/FormError.jsx
@@ -1,17 +1,21 @@
-import './FormError.scss';
-import ErrorIcon from '../../assets/icons/error-24px.svg';
+import "./FormError.scss";
+import ErrorIcon from "../../assets/Icons/error-24px.svg";
 
-function FormError ({ errorState, field, children }) {
-    return (
-        <>
-        {errorState && !field && (
-            <span className='input-error'>
-                <img className='input-error__icon' src={ErrorIcon} alt='ErrorIcon'></img>
-                <p className='input-error__message'>{children}</p>
-            </span>
-        )}
-        </>
-    )
+function FormError({ errorState, field, children }) {
+  return (
+    <>
+      {errorState && !field && (
+        <span className="input-error">
+          <img
+            className="input-error__icon"
+            src={ErrorIcon}
+            alt="ErrorIcon"
+          ></img>
+          <p className="input-error__message">{children}</p>
+        </span>
+      )}
+    </>
+  );
 }
 
 export default FormError;
